a565e7791d844a0fe7648895f5ed6972c9a47934
macsandoval1999 <msandoval2@byupathway.edu>
2026-03-07 23:00:33 -0700

    First push, includes everything from the week 1 activity. Testing to see if it works on render

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const router = express.Router();
+/*********************************************
+ -Importing Modules and Setting Up Router-
+ This section imports the Express framework and creates a new router object. The router will be used to define routes for the application, allowing us to handle HTTP requests and serve responses to the client.
+ *********************************************/
+const express = require('express'); // Import the Express framework: The require function is used to include the Express module in the application, allowing us to create an Express application and use its features. This just imports the Express library so we can use it to create our server and define routes. In other words, we store the Express module in a variable called express.
+const router = express.Router(); // Create a new router object: With our stored express module, we can create a new router object using express.Router() and store it in a variable called router. This router will be used to define routes for our application. A router in Express is a way to create modular, mountable route handlers. It helps us organize our routes and middleware in a more manageable way.
 
-// Static Routes
-// Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
 
-module.exports = router;
 
+/*********************************************
+ -Static Files-
+ This section serves static files from the "public" directory. It also sets up specific routes for CSS, JavaScript, and image files to ensure they are correctly served to the client.
+ **********************************************/
+router.use(express.static("public")); // Serve static files from the "public" directory
+router.use("/css", express.static(__dirname + "public/css")); // Serve CSS files from the "public/css" directory
+router.use("/js", express.static(__dirname + "public/js")); // Serve JavaScript files from the "public/js" directory
+router.use("/images", express.static(__dirname + "public/images")); // Serve image files from the "public/images" directory
+
+module.exports = router; 
 
 
--- server.js
+++ server.js
@@ -1,30 +1,52 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
+/*******************************************
+This server.js file is the primary file of the 
+application. It is used to control the project.
+*******************************************/
+
+
+
+/*************************
+-Require Statements-
+*************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
-/* ***********************
- * Routes
- *************************/
+
+
+/*********************************************
+-View Engine and Templates-
+This section sets up the view engine and layout support for the application. It uses EJS as the templating engine and express-ejs-layouts for layout management. The default layout file is specified as "./layouts/layout".
+**********************************************/
+app.set("view engine", "ejs"); // Set up EJS as the view engine
+app.use(expressLayouts); // Use express-ejs-layouts for layout support
+app.set("layout", "./layouts/layout"); // Set the default layout file
+
+
+
+/************************
+-Routes-
+**************************/
+// static routes
 app.use(static)
 
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
+// index route
+app.get("/", function (req, res) {
+    res.render("index", {title: "Home"})
+})
+
+/************************
+-Local Server Information-
+Values from .env (environment) file
+*************************/
 const port = process.env.PORT
 const host = process.env.HOST
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
+/****************************
+-Log statement to confirm server operation-
+******************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+    console.log(`app listening on ${host}:${port}`)
 })
